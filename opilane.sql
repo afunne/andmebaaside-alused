2db1dc742b8da88b7c974ffa1c6751223294cd6c
Hussein Tahmazov <tahmazovhussejn@gmail.com>
2025-01-28 15:57:12 +0200

    Update opilane.sql

--- opilane.sql
+++ opilane.sql
@@ -59,3 +59,53 @@ select * from opilane;
 --andmete uuendamine
 UPDATE opilane SET aadres='Tartu'
 WHERE opilaneId=3
+
+CREATE TABLE Language
+(
+ID int NOT NULL PRIMARY KEY,
+Code char(3) NOT NULL,
+Language varchar(50) NOT NULL,
+IsOfficial bit,
+Percentage smallint
+);
+select * from language;
+
+INSERT INTO Language(ID, Code, Language)
+VALUES (2, 'RUS', 'vene'), (3, 'ENG', 'inglise'),
+(4, 'DE', 'saksa')
+
+Create table keeleValik(
+keeleValikId int primary key identity(1,1),
+valikuNimetus varchar(10) not null,
+opilaneId int,
+Foreign key (opilaneId) references opilane(opilaneId),
+Language int,
+foreign key (language) references language(ID)
+)
+select * from keelevalik;
+select * from Language;
+select * from opilane;
+
+INSERT INTO keelevalik(valikuNimetus, opilaneId, Language)
+Values ('valik B', 2, 3)
+
+SELECT opilane.eesnimi, Language.Language
+FROM opilane, Language, keelevalik
+WHERE opilane.opilaneId=keelevalik.opilaneId
+AND Language.ID=keelevalik.Language
+
+Create table oppimine(
+oppimineidId int primary key identity(1,1),
+aine varchar(10) not null,
+aasta int,
+opetaja varchar(10),
+opilaneId int,
+foreign key (oppimineidId) references opilaneId(ID)
+)
+
+INSERT INTO oppimineidId(valikuNimetus, opilaneId, Language)
+Values ('valik B', 2, 3)
+
+
+select * from oppimine;
+--- vigane kood
