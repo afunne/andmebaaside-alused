13d7007f323b8d531ce75bfb06fd1d1b035b9946
Hussein Tahmazov <tahmazovhussejn@gmail.com>
2025-02-04 16:05:04 +0200

    Update Protseduur.sql

--- Protseduur.sql
+++ Protseduur.sql
@@ -97,7 +97,7 @@ DROP PROCEDURE rahvaArvuUuendus;
 
 EXEC rahvaArvuUuendus 7, 1.2;
 
------------------------------------------------------
+----------------------------------------------------- XAMPP my beloved -----------------------------------------------------
 Kasutama XAMPP/localhost
 
 Create table linn(
@@ -131,3 +131,135 @@ UPDATE linn SET rahvaArv=rahvaArv*koef
 WHERE LinnId=id;
 select * from linn;
 END
+
+
+
+
+  
+--------------------------------------------- new data base in HD quality (protsidurid) ---------------------------------------------
+CREATE TABLE opilane(
+opilaneID int primary key identity (1,1),
+eesnimi varchar(25) not null,
+perenimi varchar (25) not null,
+synniaeg date,
+stip bit,
+aadres text,
+keskmine_hinne decimal(2,1)
+)
+select * from opilane;
+--andmete lisamine tabilisse
+INSERT INTO opilane(eesnimi,
+perenimi,
+synniaeg,
+stip,
+keskmine_hinne)
+VALUES(
+'Hussein',
+'Tractovich',
+'2000-12-12',
+1,
+4.5),
+(
+'Ligma',
+'Ligmavich',
+'2000-12-12',
+1,
+3.6),
+(
+'Nikita',
+'Kitiran',
+'2000-12-12',
+0,
+2.6),
+(
+'Djanko',
+'Freeman',
+'2000-12-12',
+1,
+5.0),
+(
+'Rizza',
+'Skat',
+'2000-12-12',
+0,
+3.5)
+
+-- putting here in existing table new values
+CREATE PROCEDURE IntoNimiTab
+@enimi varchar(25),
+@pnimi varchar(25),
+@saeg date,
+@tip bit,
+@khinne decimal(2,1)
+AS
+BEGIN
+INSERT INTO opilane(eesnimi,
+perenimi,
+synniaeg,
+stip,
+keskmine_hinne)
+values (@enimi, @pnimi, @saeg, @tip, @khinne);
+
+SELECT * from opilane
+
+END;
+
+EXEC IntoNimiTab @enimi = 'Tatar', @pnimi = 'Dekar', @saeg = '2001-9-12', @tip = 1, @khinne = 4.3
+
+-- Deleting (whatever or specifited line) you want
+CREATE PROCEDURE kutsutaNimTab
+@deleteID int
+AS
+BEGIN
+SELECT * from opilane;
+DELETE FROM opilane WHERE opilaneID=@deleteID
+SELECT * from opilane;
+
+END;
+
+--droping the procuder
+DROP PROCEDURE kutsutaNimTab
+
+--show me pls
+SELECT * from opilane;
+
+--d e l e t i n g >:3
+EXEC kutsutaNimTab 3;
+
+--findind out with first letters
+CREATE PROCEDURE NimiOtsing
+@taht char(1)
+AS
+BEGIN
+SELECT * from opilane
+WHERE eesnimi LIKE @taht + '%';
+--% - kõik teised tähted
+END;
+--kutse
+EXEC NimiOtsing R;
+
+
+--tabeli uuendamine + rahvaavr kasvab 10 % värra/okay I did whatever works here ik it is not 10% but values here cna be changed
+UPDATE opilane SET keskmine_hinne=keskmine_hinne*1.1
+SELECT * FROM opilane;
+
+UPDATE opilane SET keskmine_hinne=keskmine_hinne*1.1
+WHERE opilaneId=2;
+
+SELECT * FROM opilane;
+
+
+CREATE PROCEDURE opilaneArvuUuendus
+@opilaneID int,
+@koef decimal(2,1)
+AS
+BEGIN
+SELECT * FROM opilane
+UPDATE opilane SET keskmine_hinne=keskmine_hinne*@koef
+WHERE opilaneId=opilaneId;
+select * from opilane;
+END;
+
+DROP PROCEDURE opilaneArvuUuendus;
+
+EXEC opilaneArvuUuendus 7, 1.2;
