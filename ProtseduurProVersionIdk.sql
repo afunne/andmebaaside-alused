efd0f8b6bcd56add98f24722396ca8f81f59a0da
Hussein Tahmazov <tahmazovhussejn@gmail.com>
2025-02-07 11:43:59 +0200

    Update ProtseduurProVersionIdk.sql

--- ProtseduurProVersionIdk.sql
+++ ProtseduurProVersionIdk.sql
@@ -104,3 +104,91 @@ FROM linn;
 END;
 
 EXEC kokkuRahvaarv;
+
+
+
+
+
+
+----- task -----
+CREATE DATABASE arvaLoodust;
+
+Create table puu(
+puuId int Primary Key identity(1,1),
+puudeHulk int,
+puuNimi varchar(30),
+puuKõrgus int);
+
+-- seeing it
+Select * from puu;
+-- deleting it if I am confused 
+DROP TABLE puu;
+-- I wasn't told how to add so :P
+Insert into puu(puudeHulk, puuNimi, puuKõrgus)
+values (1, 'MegaTree', 200)
+
+Insert into puu(puudeHulk, puuNimi, puuKõrgus)
+values (2, 'Honourtree', 30)
+
+Insert into puu(puudeHulk, puuNimi, puuKõrgus)
+values (3, 'ObagTree', 20)
+
+-- Created a procedure to count and calculate (what I have been told)
+CREATE PROCEDURE kokkuPuu
+
+AS
+BEGIN
+SELECT count(*) AS 'puu arv', AVG(puuKõrgus) AS 'keskimine puu kõrgus', MAX(puuKõrgus) AS 'maximum puuKõrgus', Min(puuKõrgus) AS 'minimum puuKõrgus'
+FROM puu;
+END;
+
+EXEC kokkuPuu;
+
+-- adding more tress
+CREATE PROCEDURE AddMoreTrees
+@pHulk int,
+@pnimi varchar(30),
+@pKõrgus int
+AS
+BEGIN
+Insert into puu(puudeHulk, puuNimi, puuKõrgus)
+values (@pHulk, @pnimi, @pKõrgus);
+SELECT * from puu
+
+END;
+
+-- adding more
+EXEC AddMoreTrees @pHulk = 4, @pnimi='obamium', @pKõrgus=56
+
+
+DELETE FROM puu WHERE puuID=4;
+
+--Now... DESTROYING THEM   >:3
+CREATE PROCEDURE BurningDownTheTress
+@deleteID int
+AS
+BEGIN
+SELECT * from puu;
+DELETE FROM puu WHERE puuID=@deleteID;
+SELECT * from puu;
+
+END;
+
+drop procedure BurningDownTheTress;
+
+--DESTROY
+EXEC BurningDownTheTress 5;
+
+
+
+
+
+
+
+
+----- adding into XMAAPP codes of the task -----
+Create table puu(
+puuId int Primary Key AUTO_INCREMENT,
+puudeHulk int,
+puuNimi varchar(30),
+puuKõrgus int);
